b9ef20e6316290cb03f943af3267c8b6010afd01
morphineclipse <rg.gr.18043008@gmail.com>
2026-03-01 16:57:51 +0300

    deleted duplicated fs

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 
-import fs from 'fs'
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -162,5 +160,6 @@ app.get('/healthz', (req, res) => {
 })
 
 export default app
+
 
 
